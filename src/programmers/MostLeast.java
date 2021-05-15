61cda8a4e522fddb3d2b9105995ff7cb5ebf05de
ksdhk <ksdhk@naver.com>
2021-05-15 23:52:56 +0900

    Update MostLeast.java

--- src/programmers/MostLeast.java
+++ src/programmers/MostLeast.java
@@ -1,38 +1,27 @@
 package programmers;
-
+import java.util.*;
 public class MostLeast {
 
 
         public int[] solution(int[] arr) {
-            if(arr.length==1){
-                int[] answer = new int[1];
-                answer[0] = -1;
-                return answer;
-            }
-
-            int min = 0;
-
-            for(int i=0;i<arr.length;i++){
-                if(i==0){
-                    min = arr[0];
-                }else{
-                    if(min>arr[i]){
-                        min = arr[i];
-                    };
-                }
-            };
-
-            int[] answer = new int[arr.length-1];
-            int minCnt = 0;
-            for(int i=0;i<arr.length;i++){
-                if(min == arr[i]){
-                    arr[i] = arr[i+1];
-                    minCnt++;
+            ArrayList<Integer> list = new ArrayList<>();
+            if(arr.length == 1) return new int[] {-1};
+            int min = arr[0];
+            for(int i =0; i<arr.length; i++){
+                list.add(arr[i]);
+                if(min > arr[i]){
+                    min = arr[i];
                 }
             }
-
-
+            while(list.contains(min)){
+                int idx = list.indexOf(min);
+                list.remove(idx);
+            }
+            int[] answer = new int[list.size()];
+            for(int i=0; i<list.size(); i++)
+                answer[i] = list.get(i);
             return answer;
         }
+
 
 }
