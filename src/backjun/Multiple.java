4557f9e0625acc44e067e3a8a5934d7983616557
ksdhk <ksdhk@naver.com>
2021-05-17 23:41:34 +0900

    Create Multiple.java

---
+++ src/backjun/Multiple.java
@@ -0,0 +1,30 @@
+package backjun;
+import java.util.Scanner;
+
+public class Multiple {
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+
+            while(true) {
+
+                int first = in.nextInt();
+                int second = in.nextInt();
+
+                if(first == 0 && second == 0) break;
+
+                if(second % first == 0) {
+                    System.out.println("factor");
+                }
+                else if(first % second == 0) {
+                    System.out.println("multiple");
+                }
+                else {
+                    System.out.println("neither");
+                }
+            }
+        }
+
+
+}
