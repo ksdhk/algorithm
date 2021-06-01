e47e92b3672c802e4f2ee85aca238f34a4c17880
ksdhk <ksdhk@naver.com>
2021-06-02 00:05:17 +0900

    Update StrangeChar.java

--- src/programmers/StrangeChar.java
+++ src/programmers/StrangeChar.java
@@ -2,22 +2,27 @@ package programmers;
 
 public class StrangeChar {
 
+
         public String solution(String s) {
             String answer = "";
+            int j  =0;
             for(int i=0;i<s.length();i++){
                 if(!s.substring(i,i+1).equals(" ")){
-                    if(i%2==0)
+                    if(j%2==0)
                         answer=answer+s.substring(i,i+1).toUpperCase();
                     else{
                         answer=answer+s.substring(i,i+1).toLowerCase();
                     }
+                    j++;
                 }else{
                     answer=answer+" ";
+                    j=0;
                 }
             }
 
 
             return answer;
         }
-   
+    
+
 }
