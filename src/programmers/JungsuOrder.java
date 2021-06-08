59b4a3b3fe3c4dcd1cad64196bb3ddd6da367e2d
ksdhk <ksdhk@naver.com>
2021-06-09 01:44:57 +0900

    Create JungsuOrder.java

---
+++ src/programmers/JungsuOrder.java
@@ -0,0 +1,22 @@
+package programmers;
+import java.util.Arrays;
+public class JungsuOrder {
+
+     public long solution(long n)
+        {
+            String num = String.valueOf(n);
+            String ansnum = "";
+            char [] arr = new char[num.length()];
+            for (int i = 0; i < num.length(); i++)
+            {
+                arr[i] += num.charAt(i);
+            }
+            Arrays.sort(arr);
+            for (int i = arr.length-1; i >= 0; i--)
+            {
+                ansnum += arr[i];
+            }
+            long answer = Long.parseLong(ansnum);
+            return answer;
+
+}
