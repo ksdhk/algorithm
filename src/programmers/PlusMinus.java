10f63fb40be0f971124724f39bc5b5d321c7ad39
ksdhk <ksdhk@naver.com>
2021-08-11 00:31:17 +0900

    Create PlusMinus.java

---
+++ src/programmers/PlusMinus.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class PlusMinus {
+
+        public int solution(int[] absolutes, boolean[] signs) {
+            int answer = 0;
+
+            for(int i=0;i<absolutes.length;i++){
+                if(signs[i]==true){
+                    answer+=absolutes[i];
+                }else{
+                    answer-=absolutes[i];
+                }
+            }
+            return answer;
+        }
+ 
+}
