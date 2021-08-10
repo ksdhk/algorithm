bb3325d52ef08d9e722132045b5c0e7430d0dc9f
ksdhk <ksdhk@naver.com>
2021-08-11 00:33:10 +0900

    Update PlusMinus.java

--- src/programmers/PlusMinus.java
+++ src/programmers/PlusMinus.java
@@ -6,7 +6,7 @@ public class PlusMinus {
             int answer = 0;
 
             for(int i=0;i<absolutes.length;i++){
-                if(signs[i]==true){
+                if(signs[i]==true){  
                     answer+=absolutes[i];
                 }else{
                     answer-=absolutes[i];
