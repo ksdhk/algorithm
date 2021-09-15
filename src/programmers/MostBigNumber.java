1c3fe3349f8a229ab76da3c6f91a0109bd438b4d
ksdhk <ksdhk@naver.com>
2021-09-15 23:08:17 +0900

    Create MostBigNumber.java

---
+++ src/programmers/MostBigNumber.java
@@ -0,0 +1,31 @@
+package programmers;
+import java.util.*;
+
+public class MostBigNumber {
+
+
+    class Solution {
+        public String solution(int[] numbers) {
+            String answer = "";
+            String[] str = new String[numbers.length];
+
+            for(int i = 0; i < numbers.length; i++){
+                str[i] = String.valueOf(numbers[i]);
+            }
+
+            Arrays.sort(str, new Comparator<String>() {
+                @Override
+                public int compare(String a, String b) {
+                    return (b+a).compareTo(a+b);
+                }
+            });
+
+            if (str[0].equals("0")) return "0";
+            for(String s: str) answer += s;
+
+            return answer;
+        }
+    }
+
+
+}
