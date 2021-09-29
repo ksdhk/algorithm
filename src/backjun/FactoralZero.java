a265574d0b64fa10bdc21246d0a6bca1d817a5b9
ksdhk <ksdhk@naver.com>
2021-09-29 23:37:35 +0900

    11
    33

--- src/backjun/FactoralZero.java
+++ src/backjun/FactoralZero.java
@@ -2,7 +2,6 @@ package backjun;
 import java.util.Scanner;
 public class FactoralZero {
 
-    public class Main {
 
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
@@ -17,6 +16,6 @@ public class FactoralZero {
             System.out.println(count);
         }
 
-    }
+
 
 }
