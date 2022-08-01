e3f3e2175a745c3469e51c822126d3353f29b63a
ksdhk <ksdhk@naver.com>
2022-08-02 04:32:24 +0900

    joonha

---
+++ src/backjun/Joonha.java
@@ -0,0 +1,15 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class Joonha {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String prt = in.nextLine();
+
+        System.out.println(prt+"??!");
+    }
+
+
+}
