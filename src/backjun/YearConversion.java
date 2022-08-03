ffc8441748fd9fff5d917f62734794aebfaa87d9
ksdhk <ksdhk@naver.com>
2022-08-03 23:46:34 +0900

    yaer

---
+++ src/backjun/YearConversion.java
@@ -0,0 +1,12 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class YearConversion {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Integer Year = in.nextInt();
+
+        System.out.println(Year-543);
+    }
+}
