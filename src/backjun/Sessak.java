9872bbe9999976891de3b57564a529d51cbd7ee8
ksdhk <ksdhk@naver.com>
2022-08-18 22:22:07 +0900

    Sessak

---
+++ src/backjun/Sessak.java
@@ -0,0 +1,13 @@
+package backjun;
+
+public class Sessak {
+    public static void main(String[] args) {
+        String sessak = "         ,r'\"7\n"+
+                        "r`-_   ,'  ,/\n"+
+                        " \\. \". L_r'\n"+
+                        "  `~\\/\n"+
+                        "     |\n"+
+                        "    |\n";
+        System.out.println(sessak);
+    }
+}
