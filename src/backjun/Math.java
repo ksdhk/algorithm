ce18e8f5beeea8bae9b0aeba4437d878f3230d7d
ksdhk <ksdhk@naver.com>
2022-08-24 02:29:38 +0900

    math

---
+++ src/backjun/Math.java
@@ -0,0 +1,24 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class Math {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            int N = in.nextInt();
+
+            for (int i = 2; i*i <= N; i++) {	// 또는 i * i <= N
+                while (N % i == 0) {
+                    System.out.println(i);
+                    N /= i;
+                }
+            }
+            if (N != 1) {
+                System.out.println(N);
+            }
+        }
+
+
+}
